b5a38454a158b02ea84a8bec2b36ad32b572cd28
Alessandro Menezes <alessandroasm@gmail.com>
2017-01-09 10:27:25 -0300

    Fix: decrease thread sleep to improve responsiveness

--- GenericFFBDriver/vibration/VibrationController.cpp
+++ GenericFFBDriver/vibration/VibrationController.cpp
@@ -151,7 +151,7 @@ namespace vibration {
 
 			mtxSync.unlock();
 
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
 
 		if (hHidDevice != NULL) {
